02d3ba7f7c3275452c44ed489008d3ea7870064d
Nataly <tusikf@yandex.ru>
2023-06-19 21:04:30 +0300

    done 1

--- Numder1/Program.cs
+++ Numder1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine()!;
+
+if(number.Length == 5)
+{
+    if(number[0] == number[4] && number[1] == number[3])
+    {    
+        Console.WriteLine("Введенное число является полиндромом. ");
+    }
+    else{
+        Console.WriteLine("Введенное число НЕ является полиндромом.");
+    }
+}
+else
+{
+    Console.WriteLine("Введено не пятизначное число. Конец)");
+}
